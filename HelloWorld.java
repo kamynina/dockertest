0c1e521c64094be412879dd3d81627e14e5c3e76
Darya <kamynina.d@gmail.com>
2016-11-06 17:32:59 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class JavaHelloWold {
+public class HelloWold {
   public static void main(String[] args) {
     System.out.println("hello world, docker");
     System.out.println("another string");
